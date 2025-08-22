0f7aca0dc0ccdf7fef9a6dad4c7d2cbcdff85fa7
Tushar Mittal <tushar.mittal@qlik.com>
2025-08-22 14:45:41 +0530

    added get_job_categories stream

--- tap_workday/streams/__init__.py
+++ tap_workday/streams/__init__.py
@@ -1,6 +1,7 @@
-from tap_workday.streams.human_resources.get_organizations import GetOrganizations,
+from tap_workday.streams.human_resources.get_organizations import GetOrganizations
+from tap_workday.streams.human_resources.get_job_categories import GetJobCategories
 
 STREAMS = {
     "get_organizations": GetOrganizations,
-    # "get_employee": GetEmployee
+    "get_job_categories": GetJobCategories
 }
