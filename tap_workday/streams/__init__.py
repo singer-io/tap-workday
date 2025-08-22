458f0ba3093e6a60d0ca7fc2ff11d2d20b1a6538
Tushar Mittal <tushar.mittal@qlik.com>
2025-08-22 12:29:26 +0530

    refactoring by service

--- tap_workday/streams/__init__.py
+++ tap_workday/streams/__init__.py
@@ -1,5 +1,6 @@
-from tap_workday.streams.get_organizations import GetOrganizations
+from tap_workday.streams.human_resources.get_organizations import GetOrganizations,
 
 STREAMS = {
     "get_organizations": GetOrganizations,
+    # "get_employee": GetEmployee
 }
