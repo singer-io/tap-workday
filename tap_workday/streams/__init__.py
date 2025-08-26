5c4e8dfad85d1709707e22e990dab1ce79851aaa
Tushar Mittal <tushar.mittal@qlik.com>
2025-08-26 14:20:39 +0530

    performance_management streams added

--- tap_workday/streams/__init__.py
+++ tap_workday/streams/__init__.py
@@ -1,5 +1,11 @@
 from tap_workday.streams.absence_management import OverrideBalances, AbsenceInputs
 from tap_workday.streams.human_resources import Organizations, JobCategories
+from tap_workday.streams.performance_management import (
+    CertificationIssuers,
+    Competencies,
+    CompetencyCategories,
+    Degrees,
+)
 
 STREAMS = {
     # Absence_Management
@@ -8,4 +14,9 @@ STREAMS = {
     # Human_Resources
     "human_resources_job_categories": JobCategories,
     "human_resources_organizations": Organizations,
+    # Performance_Management
+    "performance_management_certification_issuers": CertificationIssuers,
+    "performance_management_competencies": Competencies,
+    "performance_management_competency_categories": CompetencyCategories,
+    "performance_management_degrees": Degrees,
 }
---
+++ tap_workday/streams/performance_management.py
@@ -0,0 +1,124 @@
+from zeep.helpers import serialize_object
+
+from tap_workday.streams.abstracts import FullTableStream
+from tap_workday.streams.common import get_workday_client, emit_full_table
+
+
+class CertificationIssuers(FullTableStream):
+    tap_stream_id = "certification_issuers"
+    replication_method = "FULL_TABLE"
+    key_properties = ["ID"]
+    data_key = "Certification_Issuer"
+
+    def get_client(self):
+        cfg = self.client.config
+        return get_workday_client(
+            tenant=cfg["tenant"],
+            username=cfg["username"],
+            password=cfg["password"],
+            service="Performance_Management",
+            version="v44.2",
+        )
+
+    def sync(self, state, transformer, parent_obj=None):
+        client = self.get_client()
+        response = client.service.Get_Certification_Issuers()
+        serialized = serialize_object(response)
+        records = serialized.get("Response_Data", {}).get(self.data_key, [])
+        return emit_full_table(self, records)
+
+
+class Certifications(FullTableStream):
+    tap_stream_id = "certifications"
+    replication_method = "FULL_TABLE"
+    key_properties = ["ID"]
+    data_key = "Certification"
+
+    def get_client(self):
+        cfg = self.client.config
+        return get_workday_client(
+            tenant=cfg["tenant"],
+            username=cfg["username"],
+            password=cfg["password"],
+            service="Performance_Management",
+            version="v44.2",
+        )
+
+    def sync(self, state, transformer, parent_obj=None):
+        client = self.get_client()
+        response = client.service.Get_Certifications()
+        serialized = serialize_object(response)
+        records = serialized.get("Response_Data", {}).get(self.data_key, [])
+        return emit_full_table(self, records)
+
+
+class Competencies(FullTableStream):
+    tap_stream_id = "competencies"
+    replication_method = "FULL_TABLE"
+    key_properties = ["Competency_ID"]
+    data_key = "Competency"
+
+    def get_client(self):
+        cfg = self.client.config
+        return get_workday_client(
+            tenant=cfg["tenant"],
+            username=cfg["username"],
+            password=cfg["password"],
+            service="Performance_Management",
+            version="v44.2",
+        )
+
+    def sync(self, state, transformer, parent_obj=None):
+        client = self.get_client()
+        response = client.service.Get_Competencies()
+        serialized = serialize_object(response)
+        records = serialized.get("Response_Data", {}).get(self.data_key, [])
+        return emit_full_table(self, records)
+
+
+class CompetencyCategories(FullTableStream):
+    tap_stream_id = "competency_categories"
+    replication_method = "FULL_TABLE"
+    key_properties = ["Code"]
+    data_key = "Competency_Category"
+
+    def get_client(self):
+        cfg = self.client.config
+        return get_workday_client(
+            tenant=cfg["tenant"],
+            username=cfg["username"],
+            password=cfg["password"],
+            service="Performance_Management",
+            version="v44.2",
+        )
+
+    def sync(self, state, transformer, parent_obj=None):
+        client = self.get_client()
+        response = client.service.Get_Competency_Categories()
+        serialized = serialize_object(response)
+        records = serialized.get("Response_Data", {}).get(self.data_key, [])
+        return emit_full_table(self, records)
+
+
+class Degrees(FullTableStream):
+    tap_stream_id = "degrees"
+    replication_method = "FULL_TABLE"
+    key_properties = ["ID"]
+    data_key = "Degree"
+
+    def get_client(self):
+        cfg = self.client.config
+        return get_workday_client(
+            tenant=cfg["tenant"],
+            username=cfg["username"],
+            password=cfg["password"],
+            service="Performance_Management",
+            version="v44.2",
+        )
+
+    def sync(self, state, transformer, parent_obj=None):
+        client = self.get_client()
+        response = client.service.Get_Degrees()
+        serialized = serialize_object(response)
+        records = serialized.get("Response_Data", {}).get(self.data_key, [])
+        return emit_full_table(self, records)
