8fc274af7a58de31cb52c54eb418614118d6b06c
Tushar Mittal <tushar.mittal@qlik.com>
2026-01-27 17:38:01 +0530

    skip tests

---
+++ tests/test_pagination.py
@@ -0,0 +1,20 @@
+import unittest
+
+from tap_tester.base_suite_tests.pagination_test import PaginationTest
+
+from .base import WorkdayBaseTest
+
+
+@unittest.skip("Skipped")
+class WorkdayPaginationTest(PaginationTest, WorkdayBaseTest):
+    """
+    Ensure tap can replicate multiple pages of data for streams that use pagination.
+    """
+
+    @staticmethod
+    def name():
+        return "tap_tester_workday_pagination_test"
+
+    def streams_to_test(self):
+        streams_to_exclude = {}
+        return self.expected_stream_names().difference(streams_to_exclude)
