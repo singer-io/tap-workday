2e60c4cdaa5f66b7bd0f6f7afd4045895ff4ef5f
Bryant Gray <bgray1028@gmail.com>
2026-05-27 10:32:47 -0400

    Bump requests to 2.33.0 and version to 0.0.2
    Fixes CVE-2026-25645 (twistlock security scan failure).

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="tap-workday",
-    version="0.0.1",
+    version="0.0.2",
     description="Singer.io tap for extracting data from workday API",
     author="Stitch",
     url="http://singer.io",
@@ -10,7 +10,7 @@ setup(
     py_modules=["tap_workday"],
     install_requires=[
         "singer-python==6.1.1",
-        "requests==2.32.5",
+        "requests==2.33.0",
         "backoff==2.2.1",
         "zeep==4.3.1",
     ],
