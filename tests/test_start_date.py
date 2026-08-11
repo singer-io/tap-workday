7e41d4add6d0287b35f262b1f643e550b587b414
Tushar Mittal <tushar.mittal@qlik.com>
2026-08-11 17:38:29 +0530

    start_date test

--- tests/test_start_date.py
+++ tests/test_start_date.py
@@ -13,17 +13,17 @@ class WorkdayStartDateTest(StartDateTest, WorkdayBaseTest):
 
     def streams_to_test(self):
         streams_to_include = {
-            "financial_management_journals",
-            "financial_management_organizations",
-            "human_resources_job_profiles",
-            "human_resources_organizations",
+            # "financial_management_journals",
+            # "financial_management_organizations",
+            # "human_resources_job_profiles",
+            # "human_resources_organizations",
             "staffing_organizations",
         }
         return self.expected_stream_names().intersection(streams_to_include)
 
     @property
     def start_date_1(self):
-        return "2020-01-01T00:00:00Z"
+        return "2019-01-01T00:00:00Z"
 
     @property
     def start_date_2(self):
