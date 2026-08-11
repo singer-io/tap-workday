a2b581107463e67c655c65bb8eb3a9dc373f06e0
Tushar Mittal <tushar.mittal@qlik.com>
2026-08-11 11:16:45 +0530

    test all fields

--- tests/test_all_fields.py
+++ tests/test_all_fields.py
@@ -17,4 +17,7 @@ class WorkdayAllFieldsStandard(AllFieldsTest, WorkdayBaseTest):
         return "tap_tester_workday_all_fields_test_standard"
 
     def streams_to_test(self):
-        return set(self.testable_streams)
+        streams_to_exclude = {
+            "human_resources_job_profiles"
+        }
+        return set(self.testable_streams).difference(streams_to_exclude)
