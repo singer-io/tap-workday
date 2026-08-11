0f962ee1fc67b13416d57ed6644cde6542b807dd
Tushar Mittal <tushar.mittal@qlik.com>
2026-08-11 10:37:40 +0530

    test start date

--- tests/test_start_date.py
+++ tests/test_start_date.py
@@ -13,7 +13,29 @@ class WorkdayStartDateTest(StartDateTest, WorkdayBaseTest):
 
     def streams_to_test(self):
         streams_to_exclude = {
+            "absence_management_absence_inputs",
+            "absence_management_override_balances",
+            "financial_management_customer_categories",
+            "financial_management_fund_hierarchies",
+            "financial_management_fund_types",
+            "financial_management_funding_sources",
+            "financial_management_funds",
+            "financial_management_journal_sources",
+            "financial_management_ledger_account_summaries",
             "financial_management_ledgers",
+            "financial_management_position_budgets",
+            "financial_management_program_hierarchies",
+            "financial_management_programs",
+            "financial_management_revenue_category_hierarchies",
+            "financial_management_spend_category_hierarchies",
+            "financial_management_supplier_categories",
+            "human_resources_job_categories",
+            "human_resources_job_family_groups",
+            "human_resources_locations",
+            "performance_management_certification_issuers",
+            "performance_management_competencies",
+            "performance_management_competency_categories",
+            "performance_management_degrees",
         }
         return self.expected_stream_names().difference(streams_to_exclude)
 
