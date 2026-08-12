6646b57f055591e92058119e25f42a587e145244
Tushar Mittal <tushar.mittal@qlik.com>
2026-08-12 13:52:01 +0530

    use stream exlusion list

--- tests/test_bookmark.py
+++ tests/test_bookmark.py
@@ -66,20 +66,42 @@ class WorkdayBookMarkTest(BookmarkTest, WorkdayBaseTest):
         }
     }
 
-
     @staticmethod
     def name():
         return "tap_tester_workday_bookmark_test"
 
     def streams_to_test(self):
-        streams_to_include = {
-            "financial_management_journals",
-            "financial_management_organizations",
-            "human_resources_job_profiles",
-            "human_resources_organizations",
-            "staffing_organizations",
+        streams_to_exclude = {
+            # full_tables
+            "absence_management_absence_inputs",
+            "absence_management_override_balances",
+            "financial_management_customer_categories",
+            "financial_management_fund_hierarchies",
+            "financial_management_fund_types",
+            "financial_management_funding_sources",
+            "financial_management_funds",
+            "financial_management_journal_sources",
+            "financial_management_ledger_account_summaries",
+            "financial_management_ledgers",
+            "financial_management_position_budgets",
+            "financial_management_program_hierarchies",
+            "financial_management_programs",
+            "financial_management_revenue_category_hierarchies",
+            "financial_management_spend_category_hierarchies",
+            "financial_management_supplier_categories",
+            "human_resources_job_categories",
+            "human_resources_job_family_groups",
+            "human_resources_locations",
+            "performance_management_certification_issuers",
+            "performance_management_competencies",
+            "performance_management_competency_categories",
+            "performance_management_degrees",
+            # Streams with no data in test account
+            "financial_management_cost_centers",
+            "financial_management_revenue_categories",
         }
-        return self.expected_stream_names().intersection(streams_to_include)
+        return set(self.testable_streams).difference(streams_to_exclude)
+
 
     def calculate_new_bookmarks(self):
         """Calculates new bookmarks by looking through sync 1 data to determine
